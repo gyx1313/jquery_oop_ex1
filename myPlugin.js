6f288c77a686789f0ae420a353737927ab0a0cd9
gyx1313 <505675397@qq.com>
2016-04-12 15:50:11 +0800

    jquery oop ex1 First Commit

---
+++ myPlugin.js
@@ -0,0 +1,13 @@
+// window.onload = function(){
+// 	$('p').css('color', 'red');
+// };
+
+;(function(){
+	var Myobj = function(eId, opts){
+		this.ele = document.getElementById(eId);
+		this.defaults = {
+			'color': 'orange',
+			
+		};
+	};
+})();
